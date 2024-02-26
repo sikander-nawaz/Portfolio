ccf2f3be65001a44fbdf06530d8c8ff3a2ebcf09
JadoGarBhai <sikandernawaz35@gmail.com>
2024-02-27 01:22:05 +0500

    'Links'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,31 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import Logo from "./Logo";
+
+const CustomLink = ({ href, title, className = "" }) => {
+  const router = useRouter();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+
+      <span
+        className={`h-[1px] inline-block bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      ></span>
+    </Link>
+  );
+};
 
 const NavBar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <Link href="/">Home</Link>
-        <Link href="/about">About</Link>
-        <Link href="/projects">Projects</Link>
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="ml-4" />
       </nav>
 
       <nav>

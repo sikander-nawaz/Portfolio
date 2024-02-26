1808a4c7f7cf4c4add8df84e7081474e6d2a905b
JadoGarBhai <sikandernawaz35@gmail.com>
2024-02-27 01:56:01 +0500

    'social_link'

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,8 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Logo from "./Logo";
+import { GithubIcon, LinkedInIcon, TwitterIcon } from "./Icons";
+import { motion } from "framer-motion";
 
 const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
@@ -28,19 +30,34 @@ const NavBar = () => {
         <CustomLink href="/projects" title="Projects" className="ml-4" />
       </nav>
 
-      <nav>
-        <Link href="/" target="_blank">
-          T
-        </Link>
-        <Link href="/" target="_blank">
-          T
-        </Link>
-        <Link href="/" target="_blank">
-          T
-        </Link>
-        <Link href="/" target="_blank">
-          T
-        </Link>
+      <nav className="flex items-center justify-center flex-wrap">
+        <motion.a
+          href="https://www.twitter.com"
+          target="_blank"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mr-3"
+        >
+          <TwitterIcon />
+        </motion.a>
+        <motion.a
+          href="https://github.com/sikander-nawaz"
+          target="_blank"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 mx-3"
+        >
+          <GithubIcon />
+        </motion.a>
+        <motion.a
+          href="https://www.linkedin.com/in/sikander-nawaz/"
+          target="_blank"
+          whileHover={{ y: -2 }}
+          whileTap={{ scale: 0.9 }}
+          className="w-6 ml-3"
+        >
+          <LinkedInIcon />
+        </motion.a>
       </nav>
 
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">

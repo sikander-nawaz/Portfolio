853cd8867afa7d096774be8288ebdf8c0fd0bbb0
JadoGarBhai <sikandernawaz35@gmail.com>
2024-02-28 20:10:40 +0500

    about

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       backgroundImage: {
         circularLight:
-          "repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 100px)",
+          "repeating-radial-gradient(rgba(0,0,0,0.4) 2px, #f5f5f5 5px, #f5f5f5 100px)",
       },
     },
   },

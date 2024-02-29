a0b57dabd7e22e74c82c6d06126ea5a162f0d9af
JadoGarBhai <sikandernawaz35@gmail.com>
2024-02-29 18:00:12 +0500

    projects

---
+++ src/pages/projects.jsx
@@ -0,0 +1,185 @@
+import React from "react";
+import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import { GithubIcon } from "@/components/Icons";
+import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+
+const FeaturedProject = ({ img, type, title, summary, github, link }) => {
+  return (
+    <article className="w-full flex items-center justify-between rounded-3xl border rounded-br-2xl border-solid border-dark bg-light shadow-2xl p-12 relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] rounded-br-3xl bg-dark" />
+
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+
+      <div className="w-1/2 flex flex-col items-start justify-between pl-16">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+        </Link>
+
+        <p className="my-2 font-medium text-dark"> {summary} </p>
+
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target="_blank"
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+          >
+            Visit Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+const Project = ({ img, type, title, github, link }) => {
+  return (
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+      <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2rem] rounded-br-3xl bg-dark" />
+
+      <Link
+        href={link}
+        target="_blank"
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+        </Link>
+
+        <div className="w-full mt-2 flex items-center justify-between">
+          <Link
+            href={link}
+            target="_blank"
+            className="underline text-lg font-semibold"
+          >
+            Visit
+          </Link>
+
+          <Link href={github} target="_blank" className="w-8">
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+const projects = () => {
+  return (
+    <>
+      <Head>
+        <title>Portfolio | Projects</title>
+        <meta name="description" content="any description" />
+      </Head>
+
+      <main className="w-full mb-16 flex flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText
+            text={"Imagination Trumps Knowledge!"}
+            className="mb-16"
+          />
+
+          <div className="grid grid-cols-12 gap-24 gap-y-32">
+            <div className="col-span-12">
+              <FeaturedProject
+                img={project1}
+                type={"Featured Project"}
+                title={"Bank_App"}
+                summary={
+                  "A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency."
+                }
+                github={"www.github.com/sikander-nawaz"}
+                link={"https://www.webapp.vercel.com/"}
+              />
+            </div>
+
+            <div className="col-span-6">
+              <Project
+                img={project1}
+                type={"Featured Project"}
+                title={"Bank_App"}
+                github={"www.github.com/sikander-nawaz"}
+                link={"https://www.webapp.vercel.com/"}
+              />
+            </div>
+
+            <div className="col-span-6">
+              {" "}
+              <Project
+                img={project1}
+                type={"Featured Project"}
+                title={"Bank_App"}
+                github={"www.github.com/sikander-nawaz"}
+                link={"https://www.webapp.vercel.com/"}
+              />
+            </div>
+
+            <div className="col-span-12">
+              {" "}
+              <FeaturedProject
+                img={project1}
+                type={"Featured Project"}
+                title={"Bank_App"}
+                summary={
+                  "A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your local currency."
+                }
+                github={"www.github.com/sikander-nawaz"}
+                link={"https://www.webapp.vercel.com/"}
+              />
+            </div>
+
+            <div className="col-span-6">
+              {" "}
+              <Project
+                img={project1}
+                type={"Featured Project"}
+                title={"Bank_App"}
+                github={"www.github.com/sikander-nawaz"}
+                link={"https://www.webapp.vercel.com/"}
+              />
+            </div>
+
+            <div className="col-span-6">
+              {" "}
+              <Project
+                img={project1}
+                type={"Featured Project"}
+                title={"Bank_App"}
+                github={"www.github.com/sikander-nawaz"}
+                link={"https://www.webapp.vercel.com/"}
+              />
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default projects;

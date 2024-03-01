bf7f796f0bf4fe10f19feb004a2540b387b6b4cf
JadoGarBhai <sikandernawaz35@gmail.com>
2024-03-01 22:53:37 +0500

    Experience_Responsive

--- src/components/LiIcons.jsx
+++ src/components/LiIcons.jsx
@@ -7,20 +7,25 @@ const LiIcons = ({ reference }) => {
     offset: ["center end", "center center"],
   });
   return (
-    <figure className="absolute left-0 stroke-dark">
-      <svg className="-rotate-90" width="75" height="75" viewBox="0 0 100 100">
+    <figure className="absolute left-0 stroke-dark dark:stroke-light">
+      <svg
+        className="-rotate-90 md:w-[60px] md:h-[60px] xs:w-[40px] xs:h-[40px]"
+        width="75"
+        height="75"
+        viewBox="0 0 100 100"
+      >
         <circle
           cx="75"
           cy="50"
           r="20"
-          className="stroke-primary stroke-1 fill-none"
+          className="stroke-primary stroke-1 fill-none dark:stroke-primaryDark"
         />
 
         <motion.circle
           cx="75"
           cy="50"
           r="20"
-          className="stroke-[5px] fill-light"
+          className="stroke-[5px] fill-light dark:fill-dark"
           style={{
             pathLength: scrollYProgress,
           }}
@@ -30,7 +35,7 @@ const LiIcons = ({ reference }) => {
           cx="75"
           cy="50"
           r="10"
-          className="animate-pulse stroke-1 fill-primary"
+          className="animate-pulse stroke-1 fill-primary dark:fill-primaryDark"
         />
       </svg>
     </figure>
